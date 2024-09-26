18aa11bddd999534906175aa9a160bc919c840a2
Andrew Steadman <ajsteadman89@gmail.com>
2024-09-25 20:32:33 -0500

    administrate config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+
+      root to: "users#index"
+    end
   devise_for :users, controllers: { sessions: 'devise/sessions' }
 
   # Reveal health status on /up
