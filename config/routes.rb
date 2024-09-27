f8b15b6ba0df55b4a60d27ab5753497a4cf420a6
Andrew Steadman <ajsteadman89@gmail.com>
2024-09-26 21:08:25 -0500

    added logo

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  # Default root route for all users
+  root to: 'home#index' # This will handle both authenticated and unauthenticated cases
+
   # Unauthenticated root route
   unauthenticated do
     root 'home#index', as: :unauthenticated_root
