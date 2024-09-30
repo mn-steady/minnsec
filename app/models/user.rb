4847e5e08bf308533397057a090ef75c386536cc
Andrew Steadman <ajsteadman89@gmail.com>
2024-09-30 18:25:32 -0500

    updated blog post views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Association with BlogPost
+  has_many :blog_posts, dependent: :destroy
+
   def admin?
     admin # This will return true or false based on the admin attribute
   end
