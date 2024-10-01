e8e5f3c8553e4e31d93f843d4cc4f5f3325586df
Andrew Steadman <ajsteadman89@gmail.com>
2024-09-30 21:36:42 -0500

    updated blog and user tests

--- test/controllers/blog_posts_controller_test.rb
+++ test/controllers/blog_posts_controller_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class BlogPostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers # Include Devise helpers
+
   setup do
     @blog_post = blog_posts(:one)
+    @user = users(:one) # Load a user fixture
+    sign_in @user       # Sign in the user before running the test
   end
 
   test "should get index" do
