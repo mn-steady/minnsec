a4482a51fe34490e82fbab26b22cd742a92671e5
Andrew Steadman <ajsteadman89@gmail.com>
2024-09-30 23:20:20 -0500

    fixed lint issues

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,6 +1,6 @@
 class BlogPostsController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :new, :create]
-  before_action :require_admin, only: [:new, :create]
+  before_action :authenticate_user!, only: [ :index, :new, :create ]
+  before_action :require_admin, only: [ :new, :create ]
 
   def index
     @blog_posts = BlogPost.order(created_at: :desc) # Display blog posts by newest first
@@ -19,7 +19,7 @@ class BlogPostsController < ApplicationController
     @blog_post.user = current_user # Associate the blog post with the current user
 
     if @blog_post.save
-      redirect_to blog_posts_path, notice: 'Blog post was successfully created.'
+      redirect_to blog_posts_path, notice: "Blog post was successfully created."
     else
       render :new
     end
@@ -33,7 +33,7 @@ class BlogPostsController < ApplicationController
 
   def require_admin
     unless current_user.admin?
-      redirect_to root_path, alert: 'You are not authorized to perform this action.'
+      redirect_to root_path, alert: "You are not authorized to perform this action."
     end
   end
 end
