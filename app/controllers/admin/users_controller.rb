0f04afcaac137edfbe1506bd20c769e5d80d7aa8
Andrew Steadman <ajsteadman89@gmail.com>
2024-09-30 21:40:12 -0500

    remove security vuln

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,35 +2,32 @@ module Admin
   class UsersController < Admin::ApplicationController
     def resource_params
       if action_name == "create"
-        # Require all fields when creating a new user
-        params.require(:user).permit(
-          :email,
-          :first_name,
-          :last_name,
-          :display_name,
-          :phone_number,
-          :admin,
-          :password,
-          :password_confirmation
-        )
+        create_params
       else
-        # Allow updates without requiring any fields
-        permitted_params = params.require(:user).permit(
-          :email,
-          :first_name,
-          :last_name,
-          :display_name,
-          :phone_number,
-          :admin
-        )
-
-        # Conditionally allow password updates only if provided
-        if params[:user][:password].present? || params[:user][:password_confirmation].present?
-          permitted_params.merge!(params.require(:user).permit(:password, :password_confirmation))
-        end
-
-        permitted_params
+        update_params
       end
+    end
+
+    private
+
+    # Parameters for user creation
+    def create_params
+      permitted_params = [:email, :first_name, :last_name, :display_name, :phone_number, :password, :password_confirmation]
+      permitted_params << :admin if current_user.admin? # Only allow admin to set the admin flag
+      params.require(:user).permit(permitted_params)
+    end
+
+    # Parameters for user updates
+    def update_params
+      permitted_params = [:email, :first_name, :last_name, :display_name, :phone_number]
+      permitted_params << :password << :password_confirmation if password_present?
+      permitted_params << :admin if current_user.admin? # Only allow admin to update the admin flag
+      params.require(:user).permit(permitted_params)
+    end
+
+    # Helper method to check if password fields are present during update
+    def password_present?
+      params[:user][:password].present? || params[:user][:password_confirmation].present?
     end
   end
 end
