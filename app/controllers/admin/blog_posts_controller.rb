45787b70a1e5e0f1c60cf0364fc163e99ff80755
Andrew Steadman <ajsteadman89@gmail.com>
2024-09-30 20:06:15 -0500

    updated dashboard

--- app/controllers/admin/blog_posts_controller.rb
+++ app/controllers/admin/blog_posts_controller.rb
@@ -1,46 +1,34 @@
 module Admin
   class BlogPostsController < Admin::ApplicationController
-    # Overwrite any of the RESTful controller actions to implement custom behavior
-    # For example, you may want to send an email after a foo is updated.
-    #
-    # def update
-    #   super
-    #   send_foo_updated_email(requested_resource)
-    # end
+    # Override the create action to assign the current user as the blog post's author
+    def create
+      blog_post = BlogPost.new(blog_post_params)
+      blog_post.user = current_user # Assign the current logged-in admin user as the author
+      
+      if blog_post.save
+        redirect_to admin_blog_post_path(blog_post), notice: "Blog Post was successfully created."
+      else
+        render :new
+      end
+    end
 
-    # Override this method to specify custom lookup behavior.
-    # This will be used to set the resource for the `show`, `edit`, and `update`
-    # actions.
-    #
-    # def find_resource(param)
-    #   Foo.find_by!(slug: param)
-    # end
+    # Override the update action if needed, similar to create
+    def update
+      blog_post = find_resource(params[:id])
+      blog_post.user = current_user # Ensure the current user is associated on update as well
+      
+      if blog_post.update(blog_post_params)
+        redirect_to admin_blog_post_path(blog_post), notice: "Blog Post was successfully updated."
+      else
+        render :edit
+      end
+    end
 
-    # The result of this lookup will be available as `requested_resource`
+    private
 
-    # Override this if you have certain roles that require a subset
-    # this will be used to set the records shown on the `index` action.
-    #
-    # def scoped_resource
-    #   if current_user.super_admin?
-    #     resource_class
-    #   else
-    #     resource_class.with_less_stuff
-    #   end
-    # end
-
-    # Override `resource_params` if you want to transform the submitted
-    # data before it's persisted. For example, the following would turn all
-    # empty values into nil values. It uses other APIs such as `resource_class`
-    # and `dashboard`:
-    #
-    # def resource_params
-    #   params.require(resource_class.model_name.param_key).
-    #     permit(dashboard.permitted_attributes).
-    #     transform_values { |value| value == "" ? nil : value }
-    # end
-
-    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
-    # for more information
+    # Strong parameters for blog posts
+    def blog_post_params
+      params.require(:blog_post).permit(:title, :content, :published, :user_id)
+    end
   end
 end
--- app/dashboards/blog_post_dashboard.rb
+++ app/dashboards/blog_post_dashboard.rb
@@ -2,71 +2,50 @@ require "administrate/base_dashboard"
 
 class BlogPostDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
+  # A hash that describes the type of each of the model's fields.
   ATTRIBUTE_TYPES = {
     id: Field::Number,
+    title: Field::String,
     content: Field::Text,
     published: Field::Boolean,
-    title: Field::String,
-    user: Field::BelongsTo, # Ensure we can assign a user to a blog post
+    user: Field::BelongsTo.with_options(searchable: true, searchable_field: 'email'),
     created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    updated_at: Field::DateTime
   }.freeze
 
   # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
     id
     title
-    user # Show the user associated with the blog post
     published
+    user
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     id
     title
     content
     published
-    user # Display the user
+    user
     created_at
     updated_at
   ].freeze
 
   # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
     title
     content
     published
-    user # Allow assigning a user to the blog post
+    user
   ].freeze
 
   # COLLECTION_FILTERS
-  # a hash that defines filters that can be used while searching via the search
-  # field of the dashboard.
-  #
-  # For example to add an option to search for open resources by typing "open:"
-  # in the search field:
-  #
-  #   COLLECTION_FILTERS = {
-  #     open: ->(resources) { resources.where(open: true) }
-  #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
   # Overwrite this method to customize how blog posts are displayed
   # across all pages of the admin dashboard.
-  #
-  # def display_resource(blog_post)
-  #   "Blog Post ##{blog_post.id}"
-  # end
+  def display_resource(blog_post)
+    blog_post.title
+  end
 end
