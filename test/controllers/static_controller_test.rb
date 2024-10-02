3925313bbec46070eb29ad58873d4a713d48d192
Andrew Steadman <ajsteadman89@gmail.com>
2024-10-01 22:24:25 -0500

    fixed lint issues

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class StaticControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
-    get about_path 
+    get about_path
     assert_response :success
   end
 end
